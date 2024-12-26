bd4ed50cf12c8f45d0532221b76b0f7b9596339b
sanjay-bugbusters <sanjay.a.sahu@accenture.com>
2024-12-26 17:21:16 +0000

    Initialized the Spring-boot application with a healthcheck endpoint

---
+++ backend/src/main/java/com/bugbusters/prototype/BugBustersApplication.java
@@ -0,0 +1,13 @@
+package com.bugbusters.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugBustersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugBustersApplication.class, args);
+	}
+
+}
