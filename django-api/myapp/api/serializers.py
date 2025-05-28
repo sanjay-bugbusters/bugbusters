2d3e1cf59ca16802afda5cde3fb56f062cdb5de7
aadhavanr <aadhavanr10@gmail.com>
2025-05-28 20:24:36 +0530

    deleted unused folders

--- django-api/myapp/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Issue
-
-# Serializer to convert Issue model data to JSON
-class IssueSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Issue
-        fields = '__all__'  # Include all fields in the response
--- frontend/ReadMe.md.txt
+++
@@ -1,1 +0,0 @@
-This directory contains source code related to frontend. //
